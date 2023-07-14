21d6c1c1a5cb45e191860a3b588b0c35a9e2ac66
Dariia Paliy <dariia.palij@gmail.com>
2023-07-14 14:48:19 +0300

    renamed classes in section 1 && added section 2 + animation script

--- js/script.js
+++ js/script.js
@@ -13,3 +13,38 @@ function typeText() {
 }
 
 typeText();
+
+
+// -----------------ANIMATION ON SCROLL--------------------
+
+const animItems = document.querySelectorAll('.anim-items');
+
+if(animItems.length > 0){
+    window.addEventListener('scroll', animOnscroll)
+    function animOnscroll(){
+        for(let i = 0; i < animItems.length; i++){
+            const animItem = animItems[i],
+                animItemHeight = animItem.offsetHeight,
+                animItemOffset = offset(animItem).top,
+                animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)){
+                animItem.classList.add('active');
+            } else{
+                animItem.classList.remove('active');
+            }
+        }
+    }
+
+    function offset(el){
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+            scrollTop = window.scrollY || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+}
