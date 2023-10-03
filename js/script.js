ed1df92a5b229c4f63f901704c04077b27a52437
Dariia Paliy <dariia.palij@gmail.com>
2023-10-03 10:09:41 +0300

    changed animation in 7 section

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ textElement.innerHTML = "";
 
 let index = 0;
 function typeText() {
-    if (index < text.length) {
-        textElement.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeText, 100);
-    }
+   if (index < text.length) {
+      textElement.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeText, 100);
+   }
 }
 
 typeText();
@@ -17,103 +17,97 @@ typeText();
 // -----------------ANIMATION ON SCROLL--------------------
 
 const animItems = document.querySelectorAll(".anim-items");
-const fullpage = document.querySelector('.fullpage-scroll')
+const fullpage = document.querySelector(".fullpage-scroll");
 let isIncreased = false;
 
 if (animItems.length > 0) {
-    fullpage.addEventListener("scroll", animOnscroll);
-    function animOnscroll() {
-        for (let i = 0; i < animItems.length; i++) {
-            const animItem = animItems[i],
-                animItemHeight = animItem.offsetHeight,
-                animItemOffset = offset(animItem).top,
-                animStart = 4;
+   fullpage.addEventListener("scroll", animOnscroll);
+   function animOnscroll() {
+      for (let i = 0; i < animItems.length; i++) {
+         const animItem = animItems[i],
+            animItemHeight = animItem.offsetHeight,
+            animItemOffset = offset(animItem).top,
+            animStart = 4;
 
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint =
-                    window.innerHeight - window.innerHeight / animStart;
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+
+         if (
+            scrollY > animItemOffset - animItemPoint &&
+            scrollY < animItemOffset + animItemHeight
+         ) {
+            animItem.classList.add("active");
+            countTo(80, "counter1");
+            countTo(75, "counter2");
+            countTo(80, "counter3");
+            countTo(55, "counter4");
+            countTo(95, "counter5");
+            if (!isIncreased) {
+               increase();
+               isIncreased = true;
             }
-
-            if (
-                scrollY > animItemOffset - animItemPoint &&
-                scrollY < animItemOffset + animItemHeight
-            ) {
-                animItem.classList.add("active");
-                countTo(80, 'counter1');
-                countTo(75, 'counter2');
-                countTo(80, 'counter3');
-                countTo(55, 'counter4');
-                countTo(95, 'counter5');
-                if (!isIncreased) {
-                    increase();
-                    isIncreased = true;
-                }
-            } else {
-                if (!animItem.classList.contains("anim-stop")) {
-                    animItem.classList.remove("active");
-                }
+         } else {
+            if (!animItem.classList.contains("anim-stop")) {
+               animItem.classList.remove("active");
             }
-        }
-    }
+         }
+      }
+   }
 
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-            scrollTop = window.scrollY || document.documentElement.scrollTop;
-        return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
-    }
+   function offset(el) {
+      const rect = el.getBoundingClientRect(),
+         scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+         scrollTop = window.scrollY || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+   }
 }
 
 // -----------------PROGRESS-BAR--------------------
 
 function increase() {
-    let SPEED = 40;
-    const limit1 = parseInt(document.getElementById("value1").innerHTML, 10);
-    const limit2 = parseInt(document.getElementById("value2").innerHTML, 10);
+   let SPEED = 40;
+   const limit1 = parseInt(document.getElementById("value1").innerHTML, 10);
+   const limit2 = parseInt(document.getElementById("value2").innerHTML, 10);
 
-    function animateValue1(currentValue) {
-        if (currentValue <= limit1) {
-            document.getElementById("value1").innerHTML = currentValue + "%";
-            setTimeout(function () {
-                animateValue1(currentValue + 1);
-            }, SPEED);
-        }
-    }
+   function animateValue1(currentValue) {
+      if (currentValue <= limit1) {
+         document.getElementById("value1").innerHTML = currentValue + "%";
+         setTimeout(function () {
+            animateValue1(currentValue + 1);
+         }, SPEED);
+      }
+   }
 
-    function animateValue2(currentValue) {
-        if (currentValue <= limit2) {
-            document.getElementById("value2").innerHTML = currentValue + "%";
-            setTimeout(function () {
-                animateValue2(currentValue + 1);
-            }, SPEED);
-        }
-    }
-    animateValue1(0);
-    animateValue2(0);
+   function animateValue2(currentValue) {
+      if (currentValue <= limit2) {
+         document.getElementById("value2").innerHTML = currentValue + "%";
+         setTimeout(function () {
+            animateValue2(currentValue + 1);
+         }, SPEED);
+      }
+   }
+   animateValue1(0);
+   animateValue2(0);
 }
-
-
 
 // -----------------COUNTER--------------------
 
 function countTo(target, elementId) {
-    let current = 0;
-    const step = Math.ceil(target / 100); 
+   let current = 0;
+   const step = Math.ceil(target / 100);
 
-    function updateCounter() {
-        if (current < target) {
-            current += step;
-            if (current > target) {
-                current = target;
-            }
-            document.getElementById(elementId).textContent = current;
-            setTimeout(updateCounter, 10); 
-        }
-    }
+   function updateCounter() {
+      if (current < target) {
+         current += step;
+         if (current > target) {
+            current = target;
+         }
+         document.getElementById(elementId).textContent = current;
+         setTimeout(updateCounter, 10);
+      }
+   }
 
-    updateCounter(); 
+   updateCounter();
 }
-
-
-
